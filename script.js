305cdaa18618c42220e56d48217cd90d33cebfff
rao rabi <rabirao1039@gmail.com>
2023-06-28 15:44:00 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+//local storage
+let key = prompt("Enter Key");
+let value = prompt("Enter value");
+
+
+// let key = JSON.stringify({
+//     name:'Rao Rabi',
+//     age:19,
+//     Degree: 'BSSE',
+// })
+
+// let value = 23;
+
+localStorage.setItem(key,value);
+
+console.log(`The Value at ${key} is ${localStorage.getItem(key)}`);
+
+if(key == 'red' || key == 'blue'){
+    localStorage.removeItem(key)
+}
+
+if(key == 0){
+    localStorage.clear();
+}
+
+console.log(`Length is: ${localStorage.length}`);
+
+console.log(`key is ${localStorage.key(2)}`);
+
+
+//session storage
+
+// differs from local storage in a way that if tab is closed and opened the data get removed and the rest methods are same but on referesh the data remains like local storage
+
+sessionStorage.setItem(key,value);
+if(key == 1){
+    sessionStorage.clear();
+}
